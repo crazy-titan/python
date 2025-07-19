d83c28a44bd0bdba03c244c205d0e36385a3a89f
Abhinav <abhinavspl2003@gmail.com>
2025-07-19 20:07:57 +0530

    add python syntax file

---
+++ 01_basics/hello.py
@@ -0,0 +1,7 @@
+print("hello world") 
+# english hai bhai confidence rakh.
+
+def chai(parameter):  # def is defining a function
+    print(parameter)  # what to output? its mentioned to print
+
+chai("hello brother") # calling the function to execute.
---
+++ 01_basics/hello_hi.py
@@ -0,0 +1,11 @@
+# now we will se how to import something
+# just write english bro.
+
+from hello import chai # 'hello' file se 'chai' function import kar diye be
+
+# now use that function.
+chai("angrezi aati nhi be")
+
+# here we can see a new folder name "--pycache--" is created and all syntax from
+# hello file gets executed here.
+# we will discuss all these in next stay tunned.
