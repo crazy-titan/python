28bec25e0d3a6205c60dcf98f5acba169eff5df5
Abhinav <abhinavspl2003@gmail.com>
2025-07-20 22:55:20 +0530

    add variables to file

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -5,3 +5,8 @@ def chai(parameter):  # def is defining a function
     print(parameter)  # what to output? its mentioned to print
 
 chai("hello brother") # calling the function to execute.
+
+# below code is add later , check txt file for the referance.
+chai_one = "Lemon"
+chai_two = "Masala"
+chai_three = "Ginger"
