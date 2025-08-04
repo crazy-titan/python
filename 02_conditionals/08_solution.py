9afe761f8e5d7202fe5f60af44a51dc95d85c5d4
Abhinav <abhinavspl2003@gmail.com>
2025-08-04 19:49:00 +0530

    add 2nd solution of Q8

--- 02_conditionals/08_solution.py
+++ 02_conditionals/08_solution.py
@@ -8,3 +8,19 @@ else:
     password = "Strong"
 
 print("Your password is:",password)
+
+# The second method for solving this is by using the 'len' function.
+
+password = "san@Abh4518"  # let's assume something random that you got from the user input.
+
+if len(password) < 6:
+    strength = "Weak"
+elif len(password) <= 10:
+    strength = "Medium"
+else:
+    strength = "Hard"
+
+print("Password that you have set is: ",strength)
+
+# some of you can thing that , why we are calculating len again and again.
+# so you can optimise it by initialise a new variable ie. length_pass = len(password) and then use it at the place of len(password) everywhere.
