bf0576be485744d40572f3072841eb85b83151bf
Abhinav <abhinavspl2003@gmail.com>
2025-08-04 18:41:18 +0530

    add solution of Q5

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,13 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Snowy":
+    activity = "Build a snowman"
+
+print(activity)
+
+# here you can take the input from the user and use the knowledge of strings
+# to convert the input value in desire values as per need for the program.
