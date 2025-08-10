5232a292cda0677a47ce161628147ab6e852e7dc
Abhinav <abhinavspl2003@gmail.com>
2025-08-10 08:01:11 +0530

    add solution of Q2

---
+++ 05_function/02_solution.py
@@ -0,0 +1,8 @@
+# using return in 'def' is better practise than writing print in it.
+
+def sum_of_two(numOne,numTwo):
+    return numOne + numTwo
+
+print(sum_of_two(1,4))
+
+# basic logic no extra thinking
