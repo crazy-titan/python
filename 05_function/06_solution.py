ac98cce8bbfb5ee2edc7d0860080f02965704a23
Abhinav <abhinavspl2003@gmail.com>
2025-08-10 10:36:31 +0530

    add solution of Q6

---
+++ 05_function/06_solution.py
@@ -0,0 +1,17 @@
+# lambda functions are the functions having no name and used once for some small tasks
+# most of the time lambda functions are used in frameworks 
+
+
+
+# def cube(num):
+#     return num ** 3
+
+# print(cube(3))
+# ABOVE METHOD IS USED WHEN WE WANT TO CALL THE FUNCTION AGAIN N AGAIN
+
+# see how LAMBDA is written
+
+cube = lambda num: num ** 3  # num is a parameter and lambda function is stored in another variable 'cube'
+print(cube(3))    # will output as 27
+
+# we can't use this again n again anywhere, its just used and done here only
