b055437b5b96d649713c6093aad9eda10d1edd96
Abhinav <abhinavspl2003@gmail.com>
2025-08-10 10:22:44 +0530

    add solution of Q5

---
+++ 05_function/05_solution.py
@@ -0,0 +1,5 @@
+def greet(name = "User"):
+    return "Hello " + name + " !"
+
+print(greet("TITAN"))  # print the input value 'TITAN'
+print(greet())        # print default value 'User' if no input is provided by user.
